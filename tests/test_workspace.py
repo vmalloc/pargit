5a77813b2bdad82c0849d5ca7abcf6471c42e28e
Rotem Yaari <vmalloc@gmail.com>
2021-11-08 15:17:39 +0200

    Fix TOML parsing

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -1,8 +1,13 @@
+import toml
+
+
 def test_workspace_bump(pargit):
     pargit.repo.into_rust_workspace()
 
     pargit.release_version_minor()
-    for crate in ["crate1", "crate2"]:
-        cargo_toml = (pargit.repo.path / crate / "Cargo.toml").read()
-        assert "0.1.0" not in cargo_toml
-        assert "0.2.0" in cargo_toml
+    for crate in pargit.repo.path.listdir():
+        toml_path = crate / "Cargo.toml"
+        if not toml_path.check():
+            continue
+        data = toml.loads(toml_path.read())
+        assert data["package"]["version"] == "0.2.0"
