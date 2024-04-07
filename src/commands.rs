b593a75d937255b1f14aa16b6603dfed001c3d9e
Rotem Yaari <vmalloc@gmail.com>
2024-04-07 22:18:50 +0300

    Drop "hotfix" command, add documentation

--- src/commands.rs
+++ src/commands.rs
@@ -7,6 +7,8 @@ use strum_macros::EnumString;
 #[derive(Subcommand)]
 pub enum Command {
     #[clap(verbatim_doc_comment)]
+    /// Configures the current pargit repository
+    ///
     /// Pargit has several aspects of its behavior that can be controlled and customized through an optional repository file,
     /// called .pargit.toml
     ///
@@ -31,10 +33,7 @@ pub enum Command {
     /// compilation_command = "cargo check"
     Configure,
     #[clap(subcommand)]
-    /// Manipulate hotfix releases
-    Hotfix(ReleaseCommand),
-    #[clap(subcommand)]
-    /// Release a version in one shot (branch, tag, merge and push)
+    /// Creates and manipulates releases
     Release(ReleaseCommand),
     /// Manipulate feature branches
     #[clap(subcommand)]
@@ -45,37 +44,52 @@ pub enum Command {
     #[clap(subcommand)]
     /// Manipulates versions of the current repository
     Version(VersionCommand),
+    /// Cleans up the current branch if it is already merged to develop or main branches.
     Cleanup,
 }
 
 #[derive(Parser)]
 pub struct ReleaseOptions {
     #[clap(long)]
+    /// avoids pulling upstream when performing the release
     pub no_pull: bool,
 }
 
 #[derive(Subcommand)]
 pub enum ReleaseCommand {
-    /// Starts a release. This creates a dedicated branch with the release name
+    /// Creates a new branch for release
     Start {
+        /// Names this new release. If "major", "minor" or "patch" are specified, the name pargit will use is a new version bumped from the current
+        /// version of the project
         spec: VersionSpec,
         #[clap(long = "from-ref")]
+        /// Starts the release branch from the specified ref (commit hash, branch name, etc.)
         from_ref: Option<String>,
     },
+    /// Publishes this release to a remote branch upstream
     Publish {
+        /// The release to publish (defaults to the current branch)
         name: Option<String>,
     },
+    /// Deletes the release branch, and its published upstream branch (if one exists)
     Delete {
+        /// The name of the release branch to delete (defaults to current branch)
         name: Option<String>,
     },
+    /// Finishes a release. This tags the release branch and merges back to the main and develop branches. Once successful,
+    /// the release branch and its upstream will be deleted
     Finish {
         #[clap(flatten)]
         options: ReleaseOptions,
+        /// Name of the release branch to finalize. Defaults to current branch
         name: Option<String>,
     },
+    /// Releases a version in one shot. This means creating the branch, bumping its version as specified, and finalizing a release from it
     Version {
         #[clap(flatten)]
         options: ReleaseOptions,
+
+        /// Kind of release to perform (major, minor or patch)
         kind: BumpKind,
     },
 }
